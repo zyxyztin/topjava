69a498e7850ced3561ab9dd0196558002c950d7a
RN <nekhaenkors@gmail.com>
2020-06-11 00:07:25 +0300

    Revert "HW0 - refactored filteredByCycles and filteredByStreams"
    This reverts commit 18a7634e

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -18,13 +18,6 @@ public class MealTo {
         this.excess = excess;
     }
 
-    public UserMealWithExcess(UserMeal userMeal) {
-        this.dateTime = userMeal.getDateTime();
-        this.description = userMeal.getDescription();
-        this.calories = userMeal.getCalories();
-        this.excess = true;
-    }
-
     @Override
     public String toString() {
         return "MealTo{" +
