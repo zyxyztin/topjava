852304183872c4f000ec0dce602bcd82c655fc18
RN <nekhaenkors@gmail.com>
2020-07-22 09:50:18 +0300

    Patch 5.7 (Spring Data JPA)

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.model;
 
+import org.springframework.data.domain.Persistable;
 import org.springframework.util.Assert;
 
 import org.hibernate.Hibernate;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @MappedSuperclass
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
-public abstract class AbstractBaseEntity {
+public abstract class AbstractBaseEntity implements Persistable<Integer> {
     public static final int START_SEQ = 100000;
 
     @Id
@@ -31,6 +32,7 @@ public abstract class AbstractBaseEntity {
         this.id = id;
     }
 
+    @Override
     public Integer getId() {
         return id;
     }
@@ -41,6 +43,7 @@ public abstract class AbstractBaseEntity {
         return id;
     }
 
+    @Override
     public boolean isNew() {
         return this.id == null;
     }
